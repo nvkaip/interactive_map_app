1dea0172d807e10b9634c792f6713085f6ba133f
Nikolay Kaipetskyy <nvkaip@gmail.com>
2019-09-12 17:24:13 +0300

    beginning

---
+++ src/main/java/com/test_tasks/interactive_map_app/InteractiveMapAppApplication.java
@@ -0,0 +1,13 @@
+package com.test_tasks.interactive_map_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveMapAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteractiveMapAppApplication.class, args);
+    }
+
+}
