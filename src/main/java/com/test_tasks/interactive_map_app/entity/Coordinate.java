5fad01c06334344d21a3b3cd09fc16cad7219c54
Nikolay Kaipetskyy <nvkaip@gmail.com>
2019-09-19 13:01:35 +0300

    connected lombok

--- src/main/java/com/test_tasks/interactive_map_app/entity/Coordinate.java
+++ src/main/java/com/test_tasks/interactive_map_app/entity/Coordinate.java
@@ -1,13 +1,20 @@
 package com.test_tasks.interactive_map_app.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "coordinate")
 public class Coordinate {
@@ -16,48 +23,11 @@ public class Coordinate {
     @Column(name = "id")
     private Long id;
 
+    @NonNull
     @Column(name = "latitude", nullable = false)
     private Double latitude;
 
+    @NonNull
     @Column(name = "longitude", nullable = false)
     private Double longitude;
-
-    public Coordinate() {
-    }
-
-    public Coordinate(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coordinate that = (Coordinate) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(latitude, that.latitude) &&
-                Objects.equals(longitude, that.longitude);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, latitude, longitude);
-    }
-
-    @Override
-    public String toString() {
-        return "Coordinate{" +
-                "latitude=" + latitude +
-                ", longitude=" + longitude +
-                '}';
-    }
 }
--- src/main/java/com/test_tasks/interactive_map_app/entity/Property.java
+++ src/main/java/com/test_tasks/interactive_map_app/entity/Property.java
@@ -1,9 +1,15 @@
 package com.test_tasks.interactive_map_app.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,8 +17,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.List;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "property")
 public class Property {
@@ -21,119 +29,27 @@ public class Property {
     @Column(name = "id")
     private Long id;
 
+    @NonNull
     @Column(name = "numberOfRooms")
     private Integer numberOfRooms;
 
+    @NonNull
     @Column(name = "area")
     private Double area;
 
+    @NonNull
     @Column(name = "description")
     private String description;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> images;
 
+    @NonNull
     @Column(name = "address")
     private String address;
 
+    @NonNull
     @OneToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "coordinate_id")
     private Coordinate coordinate;
-
-    public Property() {
-    }
-
-    public Property(Integer numberOfRooms, Double area, String description, String address, Coordinate coordinate) {
-        this.numberOfRooms = numberOfRooms;
-        this.area = area;
-        this.description = description;
-        this.address = address;
-        this.coordinate = coordinate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getNumberOfRooms() {
-        return numberOfRooms;
-    }
-
-    public void setNumberOfRooms(Integer numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
-    }
-
-    public Double getArea() {
-        return area;
-    }
-
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List getImages() {
-        return images;
-    }
-
-    public void setImages(List images) {
-        this.images = images;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Coordinate getCoordinate() {
-        return coordinate;
-    }
-
-    public void setCoordinate(Coordinate coordinate) {
-        this.coordinate = coordinate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Property property = (Property) o;
-        return Objects.equals(id, property.id) &&
-                Objects.equals(numberOfRooms, property.numberOfRooms) &&
-                Objects.equals(area, property.area) &&
-                Objects.equals(description, property.description) &&
-                Objects.equals(address, property.address) &&
-                Objects.equals(coordinate, property.coordinate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, numberOfRooms, area, description, address, coordinate);
-    }
-
-    @Override
-    public String toString() {
-        return "Property{" +
-                "id=" + id +
-                ", numberOfRooms=" + numberOfRooms +
-                ", area=" + area +
-                ", description='" + description + '\'' +
-                ", address='" + address + '\'' +
-                ", coordinate=" + coordinate +
-                '}';
-    }
 }
